6b232494f2c1a14aff14829d4337afc805567479
alfredsoriano <149212261+alfredsoriano@users.noreply.github.com>
2023-11-05 19:36:38 -0900

    Update huffmanTree.h

--- huffmanTree.h
+++ huffmanTree.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
@@ -15,11 +14,18 @@ public:
 	node* newNode(char ch, int freq, node* left, node* right); //function that makes a new node
 	void createTree(); //SUBTASK B; creates a tree from the histogram
 	void createHistogram(string inputString); //SUBTASK A; calculates each char occurence in the inputString and stores it in the "histogram" map
-	void createCodes(node* node, string code); //traverses the tree, creating and storing the binary codes in the "codes" map
-	void printTree(node* node); //prints the tree using inorder traversal
+	map<char, string> createCodes(node* node, string code); //traverses the tree, creating and storing the binary codes in the "codes" map. NOTE: 2ND PARAMETER SHOULD BE "";
+	string createBinaryCode(string inputString); //takes the inputString and converts it into a binary string using the codes map
+	string decodeBinaryCode(node* root, string binaryCode); //SUBTASK C; takes in the binaryCode generated from createBinaryCode + the root node, and returns the decoded string
+	string getInputString(); //returns the input string
 	node* getRoot(); //returns the root node
+	string getBinaryCode(); //returns the binary code
+	map<char, int> getHistogram(); //returns the histogram map
+	map<char, string> getCodes(); //returns the codes map
 private:
+	string inputString;
 	node* root = nullptr;
+	string binaryCode = "";
 	map<char, int> histogram;
 	map<char, string> codes;
 };
